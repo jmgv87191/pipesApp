af5e3f52c363eec4592a5dce1373a4a398ddecab
jmgv87191 <jmgv87191@gmail.com>
2024-08-27 23:18:21 -0700

    as

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, RippleModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+
+
   title = 'pipesApp';
+
+  constructor( private primeConfig: PrimeNGConfig ){}
+
+  ngOnInit(): void {
+
+    this.primeConfig.ripple = true;
+
+  }
+
+
 }
