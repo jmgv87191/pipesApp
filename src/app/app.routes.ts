0d9ac8363942127b592c831672c6ad5a1f23ae61
juan manuel <jmgv87191@gmail.com>
2024-09-03 10:15:56 -0700

    asd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MenuComponent } from './shared/components/menu/menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "menu",
+        component: MenuComponent
+    }
+];
