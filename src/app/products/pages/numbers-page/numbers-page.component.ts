17e9152afe84d5d15777a0d1da74fb095eda94cd
juan manuel <jmgv87191@gmail.com>
2024-09-05 10:34:21 -0700

    asd

--- src/app/products/pages/numbers-page/numbers-page.component.ts
+++ src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import {  PanelModule } from 'primeng/panel';
@@ -5,10 +6,13 @@ import {  PanelModule } from 'primeng/panel';
 @Component({
   selector: 'app-numbers-page',
   standalone: true,
-  imports: [PanelModule, CardModule],
+  imports: [PanelModule, CardModule, CommonModule],
   templateUrl: './numbers-page.component.html',
   styleUrl: './numbers-page.component.css'
 })
 export class NumbersPageComponent {
+
+  public totalSells:number = 256789.5568;
+  public percent: number = 0.4856;
 
 }
