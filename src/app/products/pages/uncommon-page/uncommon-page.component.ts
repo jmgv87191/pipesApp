46d1a1efc21a27154d205116b751bcee9205e559
juan Manuel <jmgv87191@gmail.com>
2024-09-10 09:08:49 -0700

    asd

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,12 +1,61 @@
 import { Component } from '@angular/core';
+import {  PanelModule } from 'primeng/panel';
+import { FieldsetModule } from 'primeng/fieldset';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+import { interval, tap } from 'rxjs';
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [],
+  imports: [PanelModule, FieldsetModule,ButtonModule, CommonModule],
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
 export class UncommonPageComponent {
+
+  public name: string = 'Juan Manuel';
+  public gender: 'male' | 'female' = 'male';
+  public invitationMap = {
+    'male':'invitartlo',
+    'female':'invitartla'
+  }
+
+  changeClient(){
+    this.name = 'Alondra';
+    this.gender = 'female';
+  }
+
+  /* i18nPlural */
+
+  public clients: string[] = [ 'Maria', 'Pedro', 'Fernando','Eduardo','Melissa', 'Natalia' ]
+
+  public clientsMap = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': 'tenemos # clientes esperando',
+
+  }
+
+  deleteClient(){
+    this.clients.pop();
+  }
+
+  /* KeyValue pipe */
+
+  public person = {
+    name: 'Juan',
+    age:37,
+    address: 'La Paz, Mexico'
+  }
+
+  /* Async pipe */
+
+  public myObservableTimer = interval( 2000 ).pipe(
+    tap( value => console.log('tap',value) )
+  );
+
+
+
 
 }
